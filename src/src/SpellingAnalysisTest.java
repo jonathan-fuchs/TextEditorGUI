5bc4632547c439db30b9e0ea5b800413d2260c2c
Adam Schneider <adamschn@seas.upenn.edu>
2019-07-23 22:12:41 -0700

    Shell for SpellingAnalysis Test

---
+++ src/src/SpellingAnalysisTest.java
@@ -0,0 +1,22 @@
+package src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+class SpellingAnalysisTest {
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void testTotalWordsMisspelled() {
+		SpellingAnalysis test = new SpellingAnalysis();
+		test.totalWordsMisspelled();
+		// test with "the cat went ouut of the dor"
+		fail("Not yet implemented");
+	}
+
+}
