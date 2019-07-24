75bf4a630e090f85d71346e4de37add99956bc92
Adam Schneider <adamschn@seas.upenn.edu>
2019-07-23 19:52:37 -0700

    Update SpellingAnalysis

--- src/SpellingAnalysis.java
+++ src/SpellingAnalysis.java
@@ -1,4 +1,28 @@
 
 public class SpellingAnalysis {
+	
+	public SpellingAnalysis() {
+		
+	}
+	
+	public double percentWordsSpelledCorrectly() {
+		
+	}
+	
+	public double percentWordedMisspelledInDocument() {
+		
+	}
+	
+	public double percentWordsRequiringManualEntry() {
+		
+	}
+	
+	public double percentWordsFromSuggestions() {
+		
+	}
+	
+	public int totalWordsMisspelled() {
+		
+	}
 
 }
