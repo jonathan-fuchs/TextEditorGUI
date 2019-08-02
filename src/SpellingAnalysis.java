4988497211bfe43f3af3933e0c6fec0680875bd4
Adam Schneider <adamschn@seas.upenn.edu>
2019-08-01 18:47:22 -0700

    Added approxReadability, getSyllablesInWord, SpellingAnalysisTest
    Still need to pull in sentence length data for readability.

--- src/SpellingAnalysis.java
+++ src/SpellingAnalysis.java
@@ -11,9 +11,10 @@ public class SpellingAnalysis {
 	
 	
 	public void spellingAnalysisSuite() {
-		System.out.println("Here is some exciting analysis on the spelling in your document: ");
+		System.out.println("Here is some exciting analysis on your document: ");
 		System.out.println();
 		spellingPercentBreakdown();
+		approxReadability();
 	}
 	
 	public void spellingPercentBreakdown() {
@@ -25,6 +26,10 @@ public class SpellingAnalysis {
 		double percentFromManualEntry = (double) this.wordsFromManualEntry / wrongWords;
 		double percentAccepted = (double) this.wordsAccepted / this.wordCount;
 		
+		System.out.println();
+		System.out.println("Spelling Breakdown");
+		System.out.println("------------------");
+		System.out.println();
 		System.out.println("Words Flagged by Spell Check: " + percentChecked + "%");
 		System.out.println("Words Spelled Incorrectly: " + percentWrong + "%");
 		System.out.println("Misspelled Words Corrected By Suggestion: " + percentFromSuggestion + "%");
@@ -44,36 +49,81 @@ public class SpellingAnalysis {
 	
 	public void approxReadability() {
 		/**
-		 * formula for readability:
+		 * formula for Flesch Reading Ease readability:
 		 *  206.835 – (1.015 x ASL) – (84.6 x ASW)
+		 *  
+		 *  The formula for the Flesch-Kincaid Grade Level score is:
+		 *  (.39 x ASL) + (11.8 x ASW) – 15.59 
+		 *  
 		 *  where:
 		 *  ASL = average sentence length (the number of words divided by the number of sentences)
 		 *  ASW = average number of syllables per word (the number of syllables divided by the number of words)
 		 */
+		double sentenceLen = 0;
+		double syllablePerWord = 0;
+		double aveSentenceLen;
+		double aveSyllablePerWord;
 		
+		double readingEase;
+		double readingGradeLevel;
 		
+		for (int sentence : this.sentenceLengths) {
+			sentenceLen += sentence;
+		}
+		aveSentenceLen = sentenceLen / this.getSentenceLengths().size();
+		
+		for (int syllables : this.syllableCounts) {
+			syllablePerWord += syllables;
+		}
+		aveSyllablePerWord = syllablePerWord / this.getSyllableCounts().size();
+		
+		readingEase = 206.835 - (1.015 * aveSentenceLen) - (84.6 * aveSyllablePerWord);
+		readingGradeLevel = (.39 * aveSentenceLen) + (11.8 * aveSyllablePerWord) - 15.59;
+		
+		System.out.println();
+		System.out.println("Readability Metrics");
+		System.out.println("-------------------");
+		System.out.println();
+		System.out.println("The Flesch Reading Ease score for this text is: " + readingEase);
+		System.out.println("This rates out of 100-points and a standard score is between 60-70");
+		System.out.println();
+		System.out.println("The Flesch Kincaid Grade Level score for this text is: " + readingGradeLevel);
+		System.out.println("This gives the grade level with a standard score between 7-8");
+		System.out.println();
 	}
 	
 	/**
-	 * There is no easy algorithm for determining the number of syllables in a word. For this calculation,
+	 * There is no easy algorithm for determining the number of syllables in a word. For this rough calculation,
 	 * using the following:
 	 * Count vowels
 	 * Remove double vowels
-	 * Disregard vowels at end of word
 	 * Also adds the count to the ArrayList, to determine the average of syllables in readability
 	 * @param word to determine syllables
 	 */
 	public void getSyllablesInWord(String word) {
 		word = word.toLowerCase();
 		char[] lettersInWord = word.toCharArray();
+		boolean lastLetterVowel = false;
+		int syllableCount = 0;
 		
 		
 		for (char letter : lettersInWord) {
-			//if (letter == aeiou || letter == aeiou || letter == aeiou || letter == aeiou ||) {
-				
-			//}
+			if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'y') {
+				if (!lastLetterVowel) {
+					syllableCount++;
+					lastLetterVowel = true;
+				}
+				else {
+					lastLetterVowel = false;
+				}
+			}
+			else {
+				lastLetterVowel = false;
+			}
 		}
+		this.syllableCounts.add(syllableCount);
 	}
+	
 	
 	public int getSentenceLength() {
 		return 0;
@@ -97,6 +147,28 @@ public class SpellingAnalysis {
 	
 	
 	
+	
+	
+	public int getWordsFromSuggestion() {
+		return wordsFromSuggestion;
+	}
+
+	public int getWordsFromManualEntry() {
+		return wordsFromManualEntry;
+	}
+
+	public int getWordsAccepted() {
+		return wordsAccepted;
+	}
+
+	public ArrayList<Integer> getSentenceLengths() {
+		return sentenceLengths;
+	}
+
+	public ArrayList<Integer> getSyllableCounts() {
+		return syllableCounts;
+	}
+
 //	public SpellingAnalysis() {
 //		
 //	}
--- src/SpellingAnalysisTest.java
+++ src/SpellingAnalysisTest.java
@@ -17,5 +17,40 @@ class SpellingAnalysisTest {
 		// test with "the cat went ouut of the dor"
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	void testGetSyllablesInWord1() {
+		SpellingAnalysis test = new SpellingAnalysis();
+		test.getSyllablesInWord("buggy");
+		assertEquals(2, test.getSyllableCounts().get(0));
+	}
+	
+	@Test
+	void testGetSyllablesInWord2() {
+		SpellingAnalysis test = new SpellingAnalysis();
+		test.getSyllablesInWord("said");
+		assertEquals(1, test.getSyllableCounts().get(0));
+	}
+	
+	@Test
+	void testGetSyllablesInWord3() {
+		SpellingAnalysis test = new SpellingAnalysis();
+		test.getSyllablesInWord("mississippi");
+		assertEquals(4, test.getSyllableCounts().get(0));
+	}
+	
+	@Test
+	void testGetSyllablesInWord4() {
+		SpellingAnalysis test = new SpellingAnalysis();
+		test.getSyllablesInWord("captain");
+		assertEquals(2, test.getSyllableCounts().get(0));
+	}
+	
+	@Test
+	void testGetSyllablesInWord5() {
+		SpellingAnalysis test = new SpellingAnalysis();
+		test.getSyllablesInWord("aptitude");
+		assertEquals(4, test.getSyllableCounts().get(0));
+	}
 
 }
